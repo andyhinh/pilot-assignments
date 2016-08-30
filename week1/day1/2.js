25441069e8cf0e5746342769768e33cb20319b5b
Andy <andywhinh@gmail.com>
2016-08-30 18:19:25 -0500

    Added missing parts of the assignment.

--- week1/day1/2.js
+++ week1/day1/2.js
@@ -2,9 +2,11 @@ var console = {log: args => args}
 
 module.exports = function fizzBuzz() {
   for (var num = 1; num < 101; num++) {
-    if (num % 3 == 0) {
+    if (num % 3 === 0 && num % 5 === 0) {
+      console.log("FizzBuzz");
+    } else if (num % 3 === 0) {
       console.log("Fizz");
-    } else if (num % 5 == 0 && num % 3 != 0) {
+    } else if (num % 5 === 0) {
       console.log("Buzz");
     } else {
       console.log(num);
